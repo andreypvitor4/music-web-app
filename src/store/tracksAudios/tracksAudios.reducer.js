6d880ef332155a72b8392631c5cab425bcf91911
Andrey Vitor <andreypvitor@gmail.com>
2021-08-22 22:11:14 -0300

    ajustes finais de codigo

--- src/store/tracksAudios/tracksAudios.reducer.js
+++ src/store/tracksAudios/tracksAudios.reducer.js
@@ -2,7 +2,9 @@ export default function tracksAudiosReducer(state = [], action) {
   switch(action.type) {
     case 'UPDATE_TRACKS_AUDIOS':
       return (
-        action.payload.length > 0 ? action.payload.map( elem => new Audio(elem.preview) ): state
+        action.payload.length > 0 ? 
+        [...action.payload.map( elem => new Audio(elem.preview) ), ...state] : 
+        state
       )
 
     default:
