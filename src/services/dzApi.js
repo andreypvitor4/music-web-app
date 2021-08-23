7872465e2a289316d523134634dc7bad43a875ae
Andrey Vitor <andreypvitor@gmail.com>
2021-08-22 21:00:51 -0300

    README, instrucoes de uso e chave da api

--- src/services/dzApi.js
+++ src/services/dzApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const dzApi = axios.create({
   baseURL: 'https://deezerdevs-deezer.p.rapidapi.com',
   headers: {
-    'x-rapidapi-key': '6f5654be33msha6c521cbe9167adp13c04djsn7f04458b9a8f',
+    'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
     'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com'
   }
 })
